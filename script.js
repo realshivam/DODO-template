daca02c2cf748ed51d600bdf6cae9e8e6599ef83
Shivam Sharma <realshivam1201@gmail.com>
2020-10-12 16:12:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+      $(document).ready(function() {
+        $(".nav-toggler").each(function(_, navToggler) {
+          var target = $(navToggler).data("target");
+          $(navToggler).on("click", function() {
+            $(target).animate({
+              height: "toggle"
+            });
+          });
+        });
+      });
+              
+
+      
+      
+   
